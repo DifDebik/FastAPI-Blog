57494ab476857289858f1a279aae6806c145e2ec
Dif <zzeni42@gmail.com>
2024-07-26 18:43:39 +0300

    Database (postgres)

--- blog/database.py
+++ blog/database.py
@@ -1,10 +1,13 @@
+import os
+import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import DB_NAME, DB_HOST, DB_PASS, DB_PORT, DB_USER
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
