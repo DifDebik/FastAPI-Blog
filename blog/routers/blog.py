aa6c0940c805a34313b14832332b3333cce965b5
Dif <zzeni42@gmail.com>
2024-08-13 01:12:52 +0300

    routing and refactoring

---
+++ blog/routers/blog.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from .. import schemas, models, database
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['Blogs']
+)
+get_db = database.get_db
+
+
+@router.get('/', response_model=list[schemas.ShowBlog])
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} is not avaialble')
+    return blog
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'Done'
+
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.update(dict(request))
+    db.commit()
+    return 'Updated'
--- blog/schemas.py
+++ blog/schemas.py
@@ -22,6 +22,3 @@ class ShowBlog(BaseModel):
     title: str
     body: str
     creator: ShowUser
-
-    class Config():
-        orm_mode = True
